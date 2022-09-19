87ecfd04134119989640cfbee2bbd869c75d8b47
sinsk23 <sinsk23@naver.com>
2022-09-19 15:14:54 +0900

    feat : main.js 생성

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const http = require("http");
+const app = express();
+const PORT = process.env.PORT || 3777;
+const HOST = process.env.HOST || 'localhost';
+
+
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("Hello Git Action!");
+  });
+
+http.createServer(app).listen(PORT,HOST, () => {
+    console.log(`Server Listening on ${HOST}:${PORT}`);
+});
+
+module.exports = app;
